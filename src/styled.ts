d63bb916ec0c78cb35cdb35ff1eca85986aaaf83
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2021-08-20 16:07:20 +0700

    Added custom scroll

--- src/styled.ts
+++ src/styled.ts
@@ -6,8 +6,28 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
 
+    overflow-y: scroll;
+
     font-family: 'Hind Siliguri', sans-serif;
 
     background-color: ${colors.dark.base};
+  }
+
+  ::-webkit-scrollbar {
+    width: 18px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #012146;
+    background-clip: padding-box;
+    border: 4px solid transparent;
+    border-radius: 24px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #011a37;
+    background-clip: padding-box;
+
+    border: 4px solid transparent;
   }
 `;
