36fd0b4988026ea45d97408c287713ceb3896e68
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2021-08-20 17:02:11 +0700

    Fixed eslint

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -2,6 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { RootState, AppDispatch } from 'store';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useTypedDispatch = () => useDispatch<AppDispatch>();
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
